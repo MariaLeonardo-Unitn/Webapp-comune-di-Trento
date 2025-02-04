80d4bbdc8c1f02022a3380785b7efbc2ec3d13ff
LorenzoDeBiasi <lorenzo.debiasi-1@studenti.unitn.it>
2025-02-04 14:30:06 +0100

    cose

--- API/ENDPOINZ/APP.js
+++ API/ENDPOINZ/APP.js
@@ -1,43 +1,40 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const App = express();
 const cors = require('cors');
+const App = express();
 require('dotenv').config();
 
 
 
 
 
-//creazione dei router
+//setting dei router
 const { router: AuthRouter, authenticateToken }= require('./ROUTERZ/Authentication');
 const CalendRouter = require('./ROUTERZ/Calendari');
 const DispRouter = require('./ROUTERZ/Disposizioni');
-//setting dei router
-App.use('/api/auth', AuthRouter);
-App.use('/api/rifiuti/calendari', authenticateToken);
-App.use('/api/rifiuti/calendari', CalendRouter);
-App.use('/api/rifiuti/disposizioni', DispRouter);
 
 //per stampare a console le richieste che vengono fatte (così per il meme)
 App.use((req, res, next) => { console.log(`${req.method} ${req.url}`); next(); });
 
 
+App.use('/api/auth', AuthRouter);
+App.use('/api/rifiuti/calendari', authenticateToken);
+App.use('/api/rifiuti/calendari', CalendRouter);
+App.use('/api/rifiuti/disposizioni', DispRouter);
 
 App.use(cors());
 App.use(express.json());
 
-
-//setting della pagina iniziale
 App.use(express.static(path.join(__dirname, '..', '..', 'FrontEnd', 'Login')));
 App.get('/', (req, res) => { res.sendFile(path.join(__dirname, '..', '..', 'FrontEnd', 'Login', 'login.html')); });
-
 
 App.get('/loadPage', (req, res) => {
     const page = req.query.page;
     const dir = req.query.Dir;
     res.sendFile(path.join(__dirname, '..', '..', 'FrontEnd', dir, page));
 });
+
 
 mongoose.connect(process.env.DB_URI)
 .then(() => { console.log('Connected to Database'); }) 
