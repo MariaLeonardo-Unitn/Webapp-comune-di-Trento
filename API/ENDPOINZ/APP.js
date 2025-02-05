821561725607584c851e609f3a3d5afe948cbb25
Gabriele Gonzato <gabrielegonzato04@gmail.com>
2025-02-05 16:53:15 +0100

    aggiunta percorsi segnalazioni e operatori

--- API/ENDPOINZ/APP.js
+++ API/ENDPOINZ/APP.js
@@ -7,9 +7,15 @@ const mongoose = require('mongoose');
 const AuthRouter = require('./ROUTERZ/Authentication');
 const CalendRouter = require('./ROUTERZ/Calendari');
 const DispRouter = require('./ROUTERZ/Disposizioni');
+const SegnRouter = require('./ROUTERZ/Segnalazioni');
+const OpDolRouter = require('./ROUTERZ/OperatoreDolomiti');
+const OpComRouter = require('./ROUTERZ/OperatoreComune');
 App.use('/api/auth', AuthRouter);
 App.use('/api/rifiuti/calendari', CalendRouter);
 App.use('/api/rifiuti/disposizioni', DispRouter);
+App.use('/api/segnalazioni', SegnRouter);
+App.use('/api/operatore_dol', OpDolRouter);
+App.use('/api/operatore_com', OpComRouter);
 
 
 mongoose.connect(process.env.DB_URI)
